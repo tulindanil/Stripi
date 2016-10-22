b6b3ad5f52236fb7961e43c3c4df5ea47717c488
tulindanil <tulindanil@gmail.com>
2016-10-22 11:10:08 +0000

    Add blue color to package

--- strip/__init__.py
+++ strip/__init__.py
@@ -1,5 +1,6 @@
 from .strip import Strip
 from .strip import Color
 from .strip import WHITE
+from .strip import BLUE
 
-__all__ = ['Strip', 'Color', 'WHITE']
+__all__ = ['Strip', 'Color', 'WHITE', 'BLUE']
