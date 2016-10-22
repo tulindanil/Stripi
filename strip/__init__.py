6f490ba5d4417d5202576a1dd492c45c8d099ffd
tulindanil <tulindanil@gmail.com>
2016-10-22 22:57:34 +0300

    Add red colour to module

--- strip/__init__.py
+++ strip/__init__.py
@@ -2,5 +2,6 @@ from .strip import Strip
 from .strip import Color
 from .strip import WHITE
 from .strip import BLUE
+from .strip import RED
 
-__all__ = ['Strip', 'Color', 'WHITE', 'BLUE']
+__all__ = ['Strip', 'Color', 'WHITE', 'BLUE', 'RED']
