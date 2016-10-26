5724fa3b16868b1739b7058deece2d573e1eedaa
tulindanil <tulindanil@gmail.com>
2016-10-26 13:37:45 +0300

    Ignore sound disabled conversations

--- vk_client.py
+++ vk_client.py
@@ -14,6 +14,8 @@ api = vk.API(session)
 def does_have_new_messages():
     dialogs = api.messages.getDialogs()
     for dialog in dialogs[1:]:
+        if 'push_settings' in dialog and dialog['push_settings']['sound'] == 0:
+            continue
         if not dialog['read_state'] and dialog['out'] == 0:
             return True
     return False
