40cc4d847c692c53eaaa01ca84e15c87d00854f8
tulindanil <tulindanil@gmail.com>
2016-10-26 12:40:00 +0300

    Add ambient mode

--- worker.py
+++ worker.py
@@ -5,6 +5,7 @@ from strip import BLUE as blue
 from strip import RED as red
 
 from vk_client import does_have_new_messages
+from watch_dog import Dog
 from threading import Thread
 
 class UnreadUpdater(Thread):
@@ -20,9 +21,16 @@ s = S()
 u = UnreadUpdater()
 u.start()
 
-is_alarm = lambda: u.new_messages == True
-is_not_alarm = lambda: not is_alarm()
+d = Dog()
+d.start()
+
+is_ambient = lambda: not d.is_available
+is_alarm = lambda: u.new_messages == True and not is_ambient()
+is_not_alarm = lambda: not is_alarm() and not is_ambient()
+
+AMBIENT = red.apply_brightness(.3)
 
 while 1:
+    s.wait(AMBIENT, predicate=is_ambient)
     s.pulse(wht, red, 1.0, predicate=is_alarm)
     s.pulse(wht, wht.apply_brightness(.3), predicate=is_not_alarm)
